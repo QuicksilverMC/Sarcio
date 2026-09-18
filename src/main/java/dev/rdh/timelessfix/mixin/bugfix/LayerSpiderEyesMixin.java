e1f2cc094fecdfecb5ae95306e3bde8673061da6
rdh <me@rdh540.dev>
2026-09-18 15:18:45 -0400

    remove unnecessary args

--- src/main/java/dev/rdh/timelessfix/mixin/bugfix/LayerSpiderEyesMixin.java
+++ src/main/java/dev/rdh/timelessfix/mixin/bugfix/LayerSpiderEyesMixin.java
@@ -2,7 +2,6 @@ package dev.rdh.timelessfix.mixin.bugfix;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.layers.LayerSpiderEyes;
-import net.minecraft.entity.monster.EntitySpider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LayerSpiderEyes.class)
 public class LayerSpiderEyesMixin {
     @Inject(method = "doRenderLayer(Lnet/minecraft/entity/monster/EntitySpider;FFFFFFF)V", at = @At("RETURN"))
-    private void tf$fixDepth(EntitySpider entitylivingbaseIn, float f, float g, float partialTicks, float h, float i, float j, float scale, CallbackInfo ci) {
+    private void tf$fixDepth(CallbackInfo ci) {
         GlStateManager.depthMask(true);
     }
 }
--- src/main/java/dev/rdh/timelessfix/mixin/bugfix/LayerWithAuraMixin.java
+++ src/main/java/dev/rdh/timelessfix/mixin/bugfix/LayerWithAuraMixin.java
@@ -2,7 +2,6 @@ package dev.rdh.timelessfix.mixin.bugfix;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.layers.LayerWitherAura;
-import net.minecraft.entity.boss.EntityWither;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LayerWitherAura.class)
 public class LayerWithAuraMixin {
     @Inject(method = "doRenderLayer(Lnet/minecraft/entity/boss/EntityWither;FFFFFFF)V", at = @At("RETURN"))
-    private void tf$fixDepth(EntityWither entitylivingbaseIn, float f, float g, float partialTicks, float h, float i, float j, float scale, CallbackInfo ci) {
+    private void tf$fixDepth(CallbackInfo ci) {
         GlStateManager.depthMask(true);
     }
 }
