dab7283f26385663b0fa95536eb19202900fb3b0
rdh <me@rdh540.dev>
2026-09-19 03:33:53 -0400

    less ahh injector

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
@@ -1,8 +1,5 @@
 package dev.rdh.sarcio.mixin.bugfix;
 
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Minecraft;
@@ -15,6 +12,7 @@ import net.minecraft.util.EnumWorldBlockLayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
@@ -30,9 +28,7 @@ public class EntityRendererMixin {
         return ret;
     }
 
-    @Definition(id = "getLightBrightness", method = "Lnet/minecraft/client/multiplayer/WorldClient;getLightBrightness(Lnet/minecraft/util/BlockPos;)F")
-    @Expression("?.?.?.getLightBrightness(@(?))")
-    @ModifyExpressionValue(method = "updateRenderer", at = @At("MIXINEXTRAS:EXPRESSION"))
+    @ModifyArg(method = "updateRenderer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;getLightBrightness(Lnet/minecraft/util/BlockPos;)F"))
     private BlockPos sarcio$fixSkyDarkening(BlockPos original) {
         return new BlockPos(this.mc.getRenderViewEntity().getPositionEyes(1.0F));
     }
