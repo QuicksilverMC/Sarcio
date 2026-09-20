67b0d7eeb531e852411fdbb1c4efe9f4386dba6c
rdh <me@rdh540.dev>
2026-09-19 22:30:54 -0400

    fix skeleton held item transform

---
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/ModelSkeletonMixin.java
@@ -0,0 +1,15 @@
+package dev.rdh.sarcio.mixin.bugfix;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelSkeleton;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ModelSkeleton.class)
+public abstract class ModelSkeletonMixin extends ModelBiped {
+    @Override
+    public void postRenderArm(float scale) {
+        this.bipedRightArm.rotationPointX++;
+        this.bipedRightArm.postRender(scale);
+        this.bipedRightArm.rotationPointX--;
+    }
+}
