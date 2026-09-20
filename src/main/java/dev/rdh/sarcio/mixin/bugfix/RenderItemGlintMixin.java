b688f25cd6dea50c4ca7647a31d7200a18c4bb3f
rdh <me@rdh540.dev>
2026-09-19 22:30:53 -0400

    size enchantment glint to the item

---
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/RenderItemGlintMixin.java
@@ -0,0 +1,17 @@
+package dev.rdh.sarcio.mixin.bugfix;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderItem.class)
+public class RenderItemGlintMixin {
+    @Inject(method = "renderItemAndEffectIntoGUI", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/RenderItem;renderItemIntoGUI(Lnet/minecraft/item/ItemStack;II)V"))
+    private void sarcio$sizeGlintToItem(ItemStack stack, int xPosition, int yPosition, CallbackInfo ci) {
+        GlStateManager.enableDepth();
+    }
+}
