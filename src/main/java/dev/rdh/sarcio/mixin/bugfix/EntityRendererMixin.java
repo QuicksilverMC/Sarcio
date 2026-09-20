17c81512b473095a41a513536298256c236e6fac
rdh <me@rdh540.dev>
2026-09-19 22:30:55 -0400

    only load spectator shaders in first person

--- src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/bugfix/EntityRendererMixin.java
@@ -5,6 +5,8 @@ import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.util.BlockPos;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,6 +16,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
+    @WrapWithCondition(method = "loadEntityShader", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/EntityRenderer;loadShader(Lnet/minecraft/util/ResourceLocation;)V"))
+    private boolean sarcio$onlyShadeFirstPerson(EntityRenderer instance, ResourceLocation shader) {
+        return this.mc.gameSettings.thirdPersonView == 0;
+    }
+
     @Shadow
     private Minecraft mc;
 
