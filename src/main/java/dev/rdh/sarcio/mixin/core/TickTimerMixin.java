a25164bd3a442de5a4ceee20480b6b7cddf098d7
rdh <me@rdh540.dev>
2026-09-25 15:00:35 -0400

    use unrounded full resolution in tick timer

--- src/main/java/dev/rdh/sarcio/mixin/core/TickTimerMixin.java
+++ src/main/java/dev/rdh/sarcio/mixin/core/TickTimerMixin.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TickTimer.class)
@@ -16,5 +17,10 @@ abstract class TickTimerMixin {
 	private void disableClockSynchronization(CallbackInfo ci) {
 		this.tickTimeCorrection = 1.0;
 		this.cumTickTime = 0L;
+	}
+
+	@ModifyVariable(method = "advance", at = @At(value = "STORE", ordinal = 0), ordinal = 0)
+	private double useNanosecondClock(double seconds) {
+		return System.nanoTime() / 1.0E9;
 	}
 }
